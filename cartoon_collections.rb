87226abdf4839ab6e8882c6d9eefead910393001
Phong Lo <phonglo724@gmail.com>
2021-02-01 00:53:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,17 +1,19 @@
-# def greet_characters(array)
-#   array.each do |greet_characters|
-#     puts "Hello #{greet_characters}!"
-#   end
-# end
+def greet_characters(array)
+  array.each do |greet_characters|
+    puts "Hello #{greet_characters}!"
+  end
+end
 
-# def list_dwarves(array)
-#   i = 0
-#   index = 0
-#   array.each_with_index { |i, index| i[index] }
-#   puts "#{i + 1}. #{array[0]}"
-#   puts "#{i + 2}. #{array[1]}"
-#   puts "#{i + 3}. #{array[2]}"
-# end
+def list_dwarves(array)
+  i = 0
+  index = 0
+  array.each_with_index { |i, index| i[index] }
+  puts "#{i + 1}. #{array[0]}"
+  puts "#{i + 2}. #{array[1]}"
+  puts "#{i + 3}. #{array[2]}"
+end
+
+#or
 
 def greet_characters(array_of_strings)
   array_of_strings.each do |elements|
